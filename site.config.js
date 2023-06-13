8f7f607fd6ada55b3bd70c5aee3d082ed40e80c1
Seongmin Cheon <47883224+seongminlab@users.noreply.github.com>
2023-06-13 18:59:14 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "K-BioX log",
+    name: "K-BioX",
     image: "/kbiox_logo.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "K-BioX PhD Students",
     bio: "Mix and Match",
